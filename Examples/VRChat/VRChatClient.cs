bb19d424481647db6ed7c1ed72026dbdc4ddad31
Yaekith <47884923+Yaekith@users.noreply.github.com>
2020-06-27 07:50:10 +0800

    In depth Explanation

--- Examples/VRChat/VRChatClient.cs
+++ Examples/VRChat/VRChatClient.cs
@@ -24,8 +24,58 @@ namespace APITests
         private void VRChatClient_JoinedRoom(PhotonClient client)
         {
             client.Instantiate(new Vector3(0f, 0.8f, 0f), new Quaternion(360f, 0.8f, 0f, 0f));
-            Console.WriteLine("HI I JOINED!");
-            Console.WriteLine(client.CurrentRoom.Players.Count);
+            Console.Title = $"PhotonAPI Tests = VRChat = Server Time: {Environment.TickCount}ms = {client.CurrentRoom.Players.Count} Player(s)";
+            var CurrentRoom = client.CurrentRoom;
+            string info = string.Empty;
+            info += $"Room: {CurrentRoom.Name}\n";
+            info += $"Player Count: {CurrentRoom.PlayerCount}\n";
+            info += $"Players in Instance:\n";
+            int Index = 0;
+            foreach (var Player in CurrentRoom.Players.Values)
+            {
+                Index++;
+                var tag = Player.CustomProperties["modTag"];
+                var user = Player.CustomProperties["user"] as Dictionary<string, object>;
+                var avatar = Player.CustomProperties["avatarDict"] as Dictionary<string, object>;
+                var avtrID = avatar["id"];
+                var userID = user["id"];
+                var username = user["username"];
+                var displayname = user["displayName"];
+                var AvatarCopying = user["allowAvatarCopying"];
+                string steamID = Player.CustomProperties["steamUserID"] == null ? "0" : Player.CustomProperties["steamUserID"].ToString();
+                if (steamID == "0" && bool.Parse(Player.CustomProperties["inVRMode"].ToString()) == false && Player.ActorNumber != client.LocalPlayer.ActorNumber)
+                {
+                    info += "================================================\n";
+                    info += $"[CHEATER] Player #{Index}:\n";
+                    info += $"[CHEATER] Player ActorID: {Player.ActorNumber}\n";
+                    info += $"[CHEATER] Player UserID: {userID}\n";
+                    info += $"[CHEATER] Player Username: {username}\n";
+                    info += $"[CHEATER] Player DisplayName: {displayname}\n";
+                    info += $"[CHEATER] Player AvatarID: {avtrID}\n";
+                    info += $"[CHEATER] Player Is Master: {Player.IsMasterClient}\n";
+                    info += $"[CHEATER] Player Is Moderator: {(tag.ToString() == string.Empty ? "False" : "True")}\n";
+                    info += $"[CHEATER] Player Has Cloning Enabled: {AvatarCopying}\n";
+                    info += $"[CHEATER] Player SteamID: {(Player.CustomProperties["steamUserID"] == null ? "0" : Player.CustomProperties["steamUserID"].ToString())}\n";
+                    info += $"[CHEATER] {username} is using a SteamID Spoofer. Cheater Detected.\n";
+                    info += "================================================\n";
+                }
+                else
+                {
+                    info += "================================================\n";
+                    info += $"Player #{Index}:\n";
+                    info += $"Player ActorID: {Player.ActorNumber}\n";
+                    info += $"Player UserID: {userID}\n";
+                    info += $"Player Username: {username}\n";
+                    info += $"Player DisplayName: {displayname}\n";
+                    info += $"Player AvatarID: {avtrID}\n";
+                    info += $"Player Is Master: {Player.IsMasterClient}\n";
+                    info += $"Player Is Moderator: {(tag.ToString() == string.Empty ? "False" : "True")}\n";
+                    info += $"Player Has Cloning Enabled: {AvatarCopying}\n";
+                    info += $"Player SteamID: {(Player.CustomProperties["steamUserID"] == null ? "0" : Player.CustomProperties["steamUserID"].ToString())}\n";
+                    info += "================================================\n";
+                }
+            }
+            Console.WriteLine(info);
         }
 
         private void VRChatClient_ConnectedToMaster(PhotonClient client)
